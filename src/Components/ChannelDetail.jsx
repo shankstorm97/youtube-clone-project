d9659797eaf6a54cf048e7773f8402907eb78915
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-02-16 16:51:51 +0530

    commit after completing channelDetail component

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,34 +1,37 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import { Videos, ChannelCard } from "./index";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  const id = useParams();
+  // const id = useParams();
+  const id = "UCmXmlB4-HJytD7wek0Uo97A";
   const [channelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([]);
-  // console.log(id);
-  // console.log(channelDetail);
 
   useEffect(() => {
-    fetchFromAPI(`channels?part=snippet&id=${id}`, "statistics").then((data) =>
-      // fetchFromAPI(`channels?id=${id}`).then((data) =>
-      // setChannelDetail(data?.items)
-      // console.log(data.pageInfo.resultsPerPage)
-      console.log(data)
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
     );
-    // fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
-    fetchFromAPI(`search`).then(
-      (data) => console.log(data)
-      // (data) => setVideos(data?.items)
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
     );
   }, [id]);
 
-  // console.log(videos);
-  return <div>ChannelDetail</div>;
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{ background: "#3f3f3f", zIndex: "10", height: "300px" }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+      </Box>
+      <Box display="flex" p={2}>
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+        {/* </Box> */}
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
-
-// https://youtube-v31.p.rapidapi.com/channels?part=snippet
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -14,6 +14,8 @@ const options = {
   },
 };
 
+
+
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   return data;
